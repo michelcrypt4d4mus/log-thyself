4188eb45febe072d6cf15cbd80368b3a2f8f38c4
michelcrypt4d4mus <michelcryptdamus@gmail.com>
2022-08-13 02:51:30 -0400

    Refactor watcher

--- lib/stream/log_file_watcher.rb
+++ lib/stream/log_file_watcher.rb
@@ -7,46 +7,16 @@ require 'pastel'
 require 'tty'
 
 class LogFileWatcher
-  attr_accessor :streamer_threads
-
-  def load_and_stream!(logfile)
+  class << self
+    attr_accessor :streamer_threads
   end
 
-  def load_and_stream!
+  def self.load_and_stream_all_open_logfiles!
+    Rails.logger.level = Logger::INFO
+    %w[logfiles logfile_lines].each { |table| ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table}") }
+
     @streamer_threads = Logfile.open_logfiles.inject({}) do |memo, logfile|
-      logfile.save!
-      memo[logfile] = {}
-      memo[logfile][:csv_lines] = logfile.write_contents_to_db!
-      Rails.logger.info("Read #{memo[logfile][:csv_lines]} lines of '#{logfile.file_path}' via CSV, launching thread to stream from here...")
-
-      memo[logfile][:thread] = Thread.new do
-        begin
-          Thread.current[:lines_written] = memo[logfile][:csv_lines]
-
-          ShellCommandStreamer.new(logfile.shell_command_to_stream).stream! do |line, line_number|
-            if line_number <= Thread.current[:lines_written]
-              Rails.logger.info("Skipping #{line_number} for #{logfile.basename} (will skip to #{Thread.current[:lines_written]}")
-            end
-
-            line = line.gsub("\u0000", '').force_encoding(Encoding::UTF_8)  # Null byte...
-
-            begin
-              LogfileLine.where(logfile_id: logfile.id, line_number: line_number, line: line).first_or_create!
-            rescue ActiveRecord::ConnectionTimeoutError => e
-              Rails.logger.warn("Connection timeout; sleeping and retrying")
-              sleep 3
-              retry
-            rescue ActiveRecord::RecordNotUnique => e
-              Rails.logger.error("#{e.class} while loading logfile #{logfile.file_path} (id: #{logfile.id}\nlineno. #{line_number}: #{line}")
-              raise e
-            end
-          end
-        rescue => e
-          Rails.logger.error "#{e.class} in thread for '#{logfile.file_path}': #{e.message}"
-          raise e
-        end
-      end
-
+      memo[logfile] = new(logfile).load_and_stream!
       memo
     end
 
@@ -78,6 +48,56 @@ class LogFileWatcher
       puts TTY::Table::Renderer::Unicode.new(table).render
       puts "\n\n\n"
       sleep(5)
+    end
+  end
+
+  def initialize(logfile)
+    logfile.save!
+    @logfile = logfile
+    @info = {}
+  end
+
+  # Returns a hash with the thread that was spawned and the lines loaded by initial CSV
+  def load_and_stream!
+    @info[:csv_lines] = @logfile.write_contents_to_db!
+    initial_lines_in_db = @logfile.logfile_lines.count
+
+    if @info[:csv_lines] != initial_lines_in_db
+      puts "WARNING [#{@logfile.id}] '#{@logfile.file_path}' claims #{@info[:csv_lines]} by us but only #{initial_lines_in_db} found in DB!"
+    end
+
+    Rails.logger.info("Read #{@info[:csv_lines]} lines of '#{@logfile.file_path}' via CSV, launching thread to stream...")
+    @info[:thread] = spawn_thread_to_read_continuously
+    @info
+  end
+
+  def spawn_thread_to_read_continuously
+    Thread.new do
+      begin
+        Thread.current[:lines_written] = @info[:csv_lines]
+
+        ShellCommandStreamer.new(@logfile.shell_command_to_stream).stream! do |line, line_number|
+          if line_number <= Thread.current[:lines_written]
+            Rails.logger.info("Skipping #{line_number} for #{@logfile.basename} (will skip to #{Thread.current[:lines_written]}")
+          end
+
+          line = line.gsub("\u0000", '').force_encoding(Encoding::UTF_8)  # Null byte...
+
+          begin
+            LogfileLine.where(logfile_id: @logfile.id, line_number: line_number, line: line).first_or_create!
+          rescue ActiveRecord::ConnectionTimeoutError => e
+            Rails.logger.warn("Connection timeout; sleeping and retrying")
+            sleep 3
+            retry
+          rescue ActiveRecord::RecordNotUnique => e
+            Rails.logger.error("#{e.class} while loading logfile #{@logfile.file_path} (id: #{@logfile.id}\nlineno. #{line_number}: #{line}")
+            raise e
+          end
+        end
+      rescue => e
+        Rails.logger.error "#{e.class} in thread for '#{@logfile.file_path}': #{e.message}"
+        raise e
+      end
     end
   end
 end
--- lib/tasks/old_log_system.thor
+++ lib/tasks/old_log_system.thor
@@ -11,7 +11,7 @@ module Collect
 
     desc 'stream', "Stream logs from the new filesystem (mostly the ones seen in Console.app)"
     def stream
-      LogFileWatcher.new.load_and_stream!
+      LogFileWatcher.load_and_stream_all_open_logfiles!
     end
   end
 end
