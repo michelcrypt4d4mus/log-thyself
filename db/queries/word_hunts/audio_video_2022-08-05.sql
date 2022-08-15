9c5d3d817a912a479f184f65f56e867f0ead99a2
michelcrypt4d4mus <michelcryptdamus@gmail.com>
2022-08-15 08:13:56 -0400

    improve word hunt query

--- db/queries/word_hunts/audio_video_2022-08-05.sql
+++ db/queries/word_hunts/audio_video_2022-08-05.sql
@@ -1,20 +1,16 @@
 SELECT
-  id,
-  log_timestamp AT TIME ZONE 'utc'::text,
-  COALESCE(event_type::VARCHAR, message_type::VARCHAR) AS "log_type",
-  event_message,
-  process_name,
-  sender_process_name,
-  category,
-  subsystem,
+  msg_type_char(message_type, event_type) AS "L",
+  log_timestamp,
+  RIGHT(process_name, 35) AS process_name,
+  RIGHT(sender_process_name, 25) AS process_name,
+  LEFT(category, 25) AS category,
+  LEFT(subsystem, 40) AS subsystem,
+  LEFT(event_message, 150)  AS event_message,
   process_id,
-  activity_identifier,
-  thread_id
+  source
 FROM macos_system_logs
 WHERE log_timestamp >= '2022-08-05T00:25:06.819Z'
-  AND log_timestamp < '2022-08-05T10:00:00'
-  AND (process_name || COALESCE(sender_process_name, '') || COALESCE(category, '') || COALESCE(subsystem, '') || COALESCE(event_message, '')) ~* '(av|audio|video|mic|viceroy|call)'
+  AND (process_name || COALESCE(sender_process_name, '') || COALESCE(category, '') || COALESCE(subsystem, '') || COALESCE(event_message, '')) ~* '(av|audio|video|record|mic|conference|viceroy|call|remote)'
   AND COALESCE(event_message, '') !~* 'VSCode'
 ORDER BY log_timestamp ASC
 LIMIT 1000
-
